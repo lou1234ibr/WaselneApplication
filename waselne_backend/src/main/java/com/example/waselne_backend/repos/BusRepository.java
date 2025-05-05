4f5caad9a973d30a118cec04510d3cef599fcc00
lou1234ibr <louanaibrahim05@gmail.com>
2025-05-05 03:29:34 +0200

    Replaced old backend with updated version

--- waselne_backend/src/main/java/com/example/waselne_backend/repos/BusRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.waselne_backend.repos;
-
-import com.example.waselne_backend.entities.Bus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusRepository extends JpaRepository<Bus, Long> {
-
-}
--- waselne_backend/src/main/java/com/example/waselne_backend/repos/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.waselne_backend.repos;
-
-import com.example.waselne_backend.entities.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
